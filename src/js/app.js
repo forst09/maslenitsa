c625092aa0afc7b719367b87dd5c95b5e7f63db1
forst09 <dforst09@gmail.com>
2023-02-21 23:29:18 +0300

    add mobile menu

--- src/js/app.js
+++ src/js/app.js
@@ -1,2 +1,19 @@
+document.addEventListener('DOMContentLoaded', function () {
+    //open mobile menu
+    let burger = document.getElementById('burger');
+    let mobileMenu = document.getElementsByClassName('mobile-menu');
+    if (burger) {
+        burger.addEventListener('click', function () {
+            mobileMenu[0].classList.add('active');
+        });
+    }
 
+    //close mobile menu
+    let close = document.getElementById('closeMobile');
+    if (close) {
+        close.addEventListener('click', function () {
+            mobileMenu[0].classList.remove('active');
+        });
+    }
+});
 
