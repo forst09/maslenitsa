c32afe0014befa34053a8bbe804e99d9494ebd8b
forst09 <dforst09@gmail.com>
2023-06-12 18:49:19 +0300

    add animations

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,8 @@
 'use strict';
+
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger.js';
+
 document.addEventListener('DOMContentLoaded', function () {
     //open mobile menu
     let burger = document.getElementById('burger');
@@ -138,5 +142,162 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     })
 
+    //gsap
+    gsap.registerPlugin(ScrollTrigger);
+
+    const tl = gsap.timeline();
+    tl.fromTo('.main-title', {
+        y: 20,
+        opacity: 0
+    }, {
+        y: 0,
+        opacity: 1,
+        duration: 1
+    })
+        .fromTo('.main__text-text', {
+            y: 20,
+            opacity: 0
+        }, {
+            y: 0,
+            opacity: 1,
+            duration: 1
+        }, 0.3)
+        .fromTo('.main__btn', {
+            y: 20,
+            opacity: 0
+        }, {
+            y: 0,
+            opacity: 1,
+            duration: 1
+        }, 0.6)
+        .fromTo('.main__date', {
+            x: -50,
+            opacity: 0
+        }, {
+            x: 0,
+            opacity: 1,
+            duration: 1
+        }, 0.9)
+        .fromTo('.logo', {
+            opacity: 0
+        }, {
+            opacity: 1,
+            duration: 1
+        }, 1)
+        .fromTo('.navigation__item', {
+            opacity: 0
+        }, {
+            opacity: 1,
+            duration: 1,
+            stagger: 0.15
+        }, 1);
+
+    gsap.from('.quote', {
+        scrollTrigger: {
+            trigger: '.main',
+            start: 'bottom 80%',
+            end: 'bottom 10%',
+            scrub: 3
+        },
+        x: '100vw',
+    });
+
+    gsap.from('.info__text', {
+        scrollTrigger: {
+            trigger: '.quote',
+            start: 'bottom 80%',
+            end: 'bottom 10%',
+            scrub: 2
+        },
+        x: '100%',
+    })
+    gsap.from('.info__photo', {
+        scrollTrigger: {
+            trigger: '.quote',
+            start: 'bottom 80%',
+            end: 'bottom 10%',
+            scrub: 2
+        },
+        x: '-100%',
+    })
+    gsap.from('.gallery__swiper', {
+        scrollTrigger: {
+            trigger: '.info',
+            start: 'bottom 80%',
+            end: 'bottom 10%',
+            scrub: 2
+        },
+        y: 100,
+        opacity: 0
+    })
+    gsap.from('.massmedia__scroll', {
+        scrollTrigger: {
+            trigger: '.gallery',
+            start: 'bottom 80%',
+            end: 'bottom 10%',
+            scrub: 2
+        },
+        x: '-100%',
+    })
+    gsap.from('.price', {
+        scrollTrigger: {
+            trigger: '.massmedia',
+            start: 'bottom 80%',
+            end: 'bottom 10%',
+            scrub: 1
+        },
+        y: 200,
+    })
+    gsap.from('.accordion', {
+        scrollTrigger: {
+            trigger: '.rools',
+            start: 'top 80%',
+            end: 'top 10%',
+            scrub: 1
+        },
+        y: 100,
+        opacity: 0,
+        stagger: 0.15
+    })
+
+    gsap.from('.video__text', {
+        scrollTrigger: {
+            trigger: '.rools',
+            start: 'bottom 50%',
+            end: 'bottom 20%',
+            scrub: 1
+        },
+        x: '100%',
+        opacity: 0
+    })
+    gsap.from('.video-js', {
+        scrollTrigger: {
+            trigger: '.rools',
+            start: 'bottom 50%',
+            end: 'bottom 20%',
+            scrub: 1
+        },
+        y: 200,
+        opacity: 0
+    })
+
+    gsap.from('.buy', {
+        scrollTrigger: {
+            trigger: '.video',
+            start: 'bottom 80%',
+            end: 'bottom 40%',
+            scrub: 2
+        },
+        x: '-100%',
+    })
+    gsap.from('.footer', {
+        scrollTrigger: {
+            trigger: '.video',
+            start: 'bottom 80%',
+            end: 'bottom 40%',
+            scrub: 2
+        },
+        x: '100%',
+    })
 });
 
